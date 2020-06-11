c82f623cb0db890022482fa57da7ac44e0b8d91d
江智鑫 <59948023@qq.com>
2020-06-11 22:15:58 +0800

    Git提交测试项目1
    Signed-off-by: 江智鑫 <59948023@qq.com>

---
+++ algo_pro1/src/test/Demo.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Demo {
+
+    public static void  main(String[] args){
+
+
+        int  num=(int)(3.40);
+        System.out.println("你好"+num);
+
+
+        float a=3.5f;
+        double b= (double) a;
+        double v = Double.parseDouble("3.5ff");
+        System.out.println(v);
+    }
+
+}
